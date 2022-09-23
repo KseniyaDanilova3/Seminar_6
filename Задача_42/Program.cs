f05bc65c1e3edd59e1d790c941285add26c3d7e6
Kseniya <pashkina.ks@mail.ru>
2022-09-23 23:17:34 +0700

    Задача_42

---
+++ Задача_42/Program.cs
@@ -0,0 +1,27 @@
+﻿// Напишите программу, которая будет преобразовывать десятичное число в двоичное.
+// 45 -> 101101; 3  -> 11; 2  -> 10
+
+Console.WriteLine("Введите число");
+int num = int.Parse(Console.ReadLine());
+
+int[] bin = new int[8];
+
+int i = 0;
+while (num > 0)
+{
+    if (num % 2 == 0)
+    {
+        bin[i] = 0;
+    }
+    else
+    {
+        bin[i] = 1;
+    }
+    num = num / 2;
+    i++;
+}
+
+for (int j = 0; j < bin.Length; j++)
+{
+    Console.Write($"{bin[(bin.Length-j-1)]}");
+}
